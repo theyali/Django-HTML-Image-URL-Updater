709efe27c7666830f07212f3f2f5d9c46f9a3b76
theyali <haciyev.ali@hotmail.com>
2023-08-02 19:54:17 +0400

    4 stage done socials

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ def pythonFunction(wildcard="*"):
 
 @eel.expose
 def update_img_src(path, substring_to_remove, images_folder):
-    print(path)
     with open(path, 'r') as file:
         content = file.read()
 
@@ -39,18 +38,38 @@ def update_img_src(path, substring_to_remove, images_folder):
 
 @eel.expose
 def update_css_src(path, substring_to_remove, images_folder):
-    print(path)
     with open(path, 'r') as file:
         content = file.read()
 
     soup = BeautifulSoup(content, 'html.parser')
-    img_tags = soup.find_all('img')
+    link_tags = soup.find_all('link', rel='stylesheet')
 
-    for img_tag in img_tags:
-        img_src = img_tag.get('src')
-        if img_src and img_src.startswith(substring_to_remove):
-            new_img_src = "{% static '" + os.path.join(images_folder, os.path.basename(img_src)) + "' %}"
-            img_tag['src'] = new_img_src
+    for link_tag in link_tags:
+        link_src = link_tag.get('src')
+        if link_src and link_src.startswith(substring_to_remove):
+            new_css_src = "{% static '" + os.path.join(images_folder, os.path.basename(link_src)) + "' %}"
+            link_tag['src'] = new_css_src
+
+    updated_content = soup.prettify()
+
+    with open(path, 'w') as file:
+        file.write(updated_content)
+
+eel.start("templates/main.html", size=(400, 400))
+
+@eel.expose
+def update_js_src(path, substring_to_remove, images_folder):
+    with open(path, 'r') as file:
+        content = file.read()
+
+    soup = BeautifulSoup(content, 'html.parser')
+    js_tags = soup.find_all('script', src=True)
+
+    for js_tag in js_tags:
+        js_src = js_tag.get('src')
+        if js_src and js_src.startswith(substring_to_remove):
+            new_css_src = "{% static '" + os.path.join(images_folder, os.path.basename(js_src)) + "' %}"
+            js_tag['src'] = new_css_src
 
     updated_content = soup.prettify()
 
