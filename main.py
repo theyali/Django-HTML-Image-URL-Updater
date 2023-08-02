3afc3023f399eb4355df06e9750d5bcdb0e098d2
theyali <haciyev.ali@hotmail.com>
2023-08-02 18:17:12 +0400

    stage 3

--- main.py
+++ main.py
@@ -37,4 +37,24 @@ def update_img_src(path, substring_to_remove, images_folder):
     with open(path, 'w') as file:
         file.write(updated_content)
 
-eel.start("main.html", size=(500, 400))
+@eel.expose
+def update_css_src(path, substring_to_remove, images_folder):
+    print(path)
+    with open(path, 'r') as file:
+        content = file.read()
+
+    soup = BeautifulSoup(content, 'html.parser')
+    img_tags = soup.find_all('img')
+
+    for img_tag in img_tags:
+        img_src = img_tag.get('src')
+        if img_src and img_src.startswith(substring_to_remove):
+            new_img_src = "{% static '" + os.path.join(images_folder, os.path.basename(img_src)) + "' %}"
+            img_tag['src'] = new_img_src
+
+    updated_content = soup.prettify()
+
+    with open(path, 'w') as file:
+        file.write(updated_content)
+
+eel.start("templates/main.html", size=(400, 400))
